e6ae233246f5e84189347d4331f507f1a8600170
David Gunawan <davidligun@gmail.com>
2016-05-03 10:04:38 +0700

    Initial Commit

---
+++ pokedex-by-dvdgn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-dvdgn
+//
+//  Created by David Gunawan on 5/3/16.
+//  Copyright © 2016 davidgun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
